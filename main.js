9715c0895738a02d73dd8934a8441de233a1e200
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-29 09:51:16 +0100

    add const for admin

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ console.log('hello node js');
 console.log(process);
 
 console.log(process.env.ADMIN_PIN);
+const admin = process.argv[2]
 
-
-if (process.argv[2]=='admin' && process.env.ADMIN_PIN==666){
+if (process.argv[2] =='admin' && process.env.ADMIN_PIN == 666){
     console.log('Welcome Admin');
 }else{
     console.log('Access restricted');
